44b4841afac18bc16167becbd6364c893ac1d892
Tulimelli Rajani <tulimellirajani@gmail.com>
2025-04-17 22:25:10 +0530

    arrays

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 MOVIES={"RRR" "BHBL" "PSPA"}
 
-echo "first movie is $(MOVIES[0])"
-echo "second movie is $(MOVIES[1])"
-echo "third movie is $(MOVIES[2])"
+echo "first movie is ${MOVIES[0]}"
+echo "second movie is ${MOVIES[1]}"
+echo "third movie is ${MOVIES[2]}"
 
-echo "all movies are $(MOVIES[@])"
+echo "all movies are ${MOVIES[@]}"
