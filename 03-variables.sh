22574a608c7c7037d2528a788a4423ef376113ef
Tulimelli Rajani <tulimellirajani@gmail.com>
2025-04-17 19:20:32 +0530

    Converstaion added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Krishiv
+Person2=Liyansh
+
+echo "$Person1: Hi $Person2"
+echo "$Person2: Hello $Person1"
+echo "$Person1: How are you doing?"
+echo "$Person2: I am doing good,How about you?"
